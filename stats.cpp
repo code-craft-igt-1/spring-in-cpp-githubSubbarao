5d87a6df188ed52bd1bf6d8ba50548a26342972e
Subba Rao <Subba.Rao@philips.com>
2024-07-24 15:38:33 +0530

    Added method implementation

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,27 @@
 #include "stats.h"
+#include <vector>
+#include <algorithm>
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
-    //Implement statistics here
+Statistics::Stats Statistics::ComputeStatistics(const std::vector<float> &numbers ) {
+    Stats statResult;
+
+    if (numbers.empty())
+    {
+        statResult.min = NAN;
+        statResult.max = NAN;
+        statResult.average = NAN;
+    }
+    else
+    {
+        //Implement statistics here
+        statResult.min = *std::min_element(numbers.begin(), numbers.end());
+        statResult.max = *std::max_element(numbers.begin(), numbers.end());
+        double sum = 0.0;
+        for (double number : numbers) {
+            sum += number;
+        }
+        statResult.average = sum / numbers.size();
+    }
+
+    return statResult;
 }
